59c0fc7ecdd5ba93178a906906ce11a3ac30520f
Viji <vijitha.wijesuriya@gmail.com>
2026-02-01 19:54:28 +0000

    update-routes

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,8 +1,7 @@
 import Home from "../pages/Home";
 import DeepDivesIndex from "../pages/DeepDivesIndex";
 import DeepDive from "../pages/DeepDive";
-
-
+export const routes = [
   { path: "/", element: <Home /> },
   { path: "/deep-dives", element: <DeepDivesIndex /> },
   { path: "/deep-dives/:slug", element: <DeepDive /> },
